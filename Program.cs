8862297fdfa9e4ecb26d68a9ac17c1abf0dc8626
cuongpham87 <lil.steven296@gmail.com>
2025-09-14 02:39:23 +0630

    add
    add

--- Program.cs
+++ Program.cs
@@ -2,6 +2,14 @@
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("xin chào các bạn");
+        int a = 5;
+        if (a == 10)
+        {
+            Console.WriteLine("biến bằng 10");
+        }
+        else
+        {
+            Console.WriteLine("Biến không bằng 10"); 
+        }
     }
 }
